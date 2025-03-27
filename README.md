e30192b6e1f9a05de13d9dab75f5749ffdeb90b0
Juliusouko <oukojulius224@gmail.com>
2025-03-27 12:08:30 +0300

    add a liking feature to all my posts

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-## me-style fashion app
+## name me-style fashion app
+
+Me style is a simple fashion interest posting application.
+
+## description
+
+## badges
+## visuals
+## installation
+## usage
+## support
--- src/index.js
+++ src/index.js
@@ -13,23 +13,76 @@ const renderPosts = async (term) => {
   const posts = await res.json();
   console.log(posts);
 
-  let template = '';
+
+
   posts.forEach(post => {
-    template += `
-    <h2>${post.title}</h2>
-      <div class="post">
-        <p>${post.body.slice(0, 50)}</p>
-        <a href="/details.html?id=${post.id}">Details</a>
-      </div>
-      <div class="likes">
-      <p><span class="material-symbols-outlined">
-      favorite</span></p>
-      <span class="likes">${post.likes}</span>
-      </div>
-    `
+    
+    let postName = document.createElement('h2')
+    postName.innerHTML = post.title
+
+    let details = document.createElement('div')
+    details.setAttribute('class', 'post')
+
+    let detailsText = document.createElement('p')
+    detailsText.innerHTML = post.body.slice(0, 50)
+
+    let detailsLink = document.createElement('a')
+    detailsLink.setAttribute('href', `/details.html?id=${post.id}`)
+
+    detailsLink.innerHTML = 'Details'
+
+    details.appendChild(detailsText)
+    details.appendChild(detailsLink)
+
+    let likesContainer = document.createElement('div')
+    likesContainer.setAttribute('class', 'likes')
+
+    //let commentContainer = document.createElement('div')
+    //commentContainer.setAttribute('class', comments)
+    //commentContainer.innerHTML = `This is a comment`
+
+    let likesText = document.createElement('p')
+    let likesIcon = document.createElement('span')
+    likesIcon.setAttribute('class','material-symbols-outlined')
+
+    likesIcon.innerHTML = 'favorite'
+
+    // add event listener on the icon 
+    likesIcon.addEventListener('click', async () => {
+      try {
+        const newLikes = parseInt(likesCount.textContent) + 1
+        likesCount.textContent = newLikes
+
+        const response = await fetch(`http://localhost:3000/posts/${post.id}`, {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({likes: newLikes})
+        })
+        if (!response.ok) {
+          throw new Error ('Failed to update likes')
+        }
+      } catch (err) {
+        console.error('Error updating likes:', err)
+      }
+    })
+
+    likesText.appendChild(likesIcon)
+
+    let likesCount = document.createElement('span')
+    likesCount.setAttribute('class', 'likes-count')
+    likesCount.innerHTML = post.likes
+
+    likesContainer.appendChild(likesText)
+    likesContainer.appendChild(likesCount)
+
+    container.appendChild(postName)
+    container.appendChild(details)
+    container.appendChild(likesContainer)
+
   });
 
-  container.innerHTML = template;
 }
 
 // search
